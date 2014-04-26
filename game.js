7cf20c9197a28882e1a40906466163147b890ed5
Shawn Krisman <ssandboxx@hotmail.com>
2014-04-26 14:14:02 -0700

    printing random tiles.

--- game.js
+++ game.js
@@ -1,8 +1,49 @@
-Game = {
-    // Initialize and start our game
-    start: function() {
-        // Start crafty and set a background color so that we can see it's working
-        Crafty.init(480, 320);
-        Crafty.background('green');
+var tilePixelWidth = 64;
+var tilePixelHeight = 64;
+var tileWidth = 15;
+var tileHeight = 8;
+
+var startGame = function() {
+    var gameWidth = tileWidth * tilePixelWidth;
+    var gameHeight = tileHeight * tilePixelHeight;
+    Crafty.init(gameWidth, gameHeight);
+    Crafty.background('green');
+    makeMap();
+}
+
+var makeMap = function() {
+    for (var x = 0; x < tilePixelWidth; ++x) {
+        for (var y = 0; y < tilePixelHeight; ++y) {
+            Crafty.e('Tile').create(x, y);
+        }
     }
+};
+
+Crafty.c('Tile', {
+    init: function() {
+        this.requires('2D, Canvas, Grid, Color');
+        this.color(randomColor());
+        this.attr({
+            w: tilePixelWidth,
+            h: tilePixelHeight,
+        })
+    },
+
+    create: function(x, y) {
+        this.attr({
+            x: x * tilePixelWidth,
+            y: y * tilePixelHeight,
+        });
+        return this;
+    }
+});
+
+var randomColor = function() {
+    var random255 = function() {
+        return Math.floor(Math.random() * 255);
+    }
+    var r = random255();
+    var g = random255();
+    var b = random255();
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')'
 }
